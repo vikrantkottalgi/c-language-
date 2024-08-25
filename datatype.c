64ea110effd3f83147d2f97ceeecb55cae66c951
vikrantkottalagi <vikrantkottalagi9417@gmail.com>
2024-08-25 09:25:57 +0530

    datatype basic file

---
+++ datatype.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n = 12;
+    char c = 'e';
+    float x = 0.23;
+    bool b = false;
+    string s = "vikrant";
+    printf("%d\n", n);
+    printf("%c\n", c);
+    printf("%f\n", x);
+    printf("%d\n", b);
+    printf("%s\n", s.c_str());
+    getch();
+}

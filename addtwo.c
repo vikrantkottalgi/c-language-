b92e7ee2bbfcc83b251197dd93fc58fd8cb9aa23
vikrantkottalagi <vikrantkottalagi9417@gmail.com>
2024-08-26 20:45:43 +0530

    loop

---
+++ addtwo.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    printf("Sum: %d\n", sum);
+    getch();
+}
---
+++ dowhile.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+void main(){
+    int a = 5;
+    do{
+        printf("%d\n", a);
+        a--;
+    }while(a != 0);
+    getch();
+}

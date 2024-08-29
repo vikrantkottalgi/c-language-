85d5ff52389c42732681b9eef2bf217ba1e12b55
vikrantkottalagi <vikrantkottalagi9417@gmail.com>
2024-08-29 20:09:52 +0530

    square

---
+++ pattern/starsquare.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int n = 4;
+    for(int i = 0; i < n; i){
+        for(int j = 0; j < n-i-1; j++){
+            printf(" *");
+        }
+    }
+    getch();
+}

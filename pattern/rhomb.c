df75e7db9ea70337b5c177bb9d8f79fd7155fd33
vikrantkottalagi <vikrantkottalagi9417@gmail.com>
2024-09-16 15:14:30 +0530

    pattern

--- pattern/rhomb.c
+++ pattern/rhomb.c
@@ -3,6 +3,9 @@ void main(){
     int n = 4;
     for(int i = 0; i < n; i){
         for(int j = 0; j < n; j++){
+            printf("  ");
+        } 
+        for(int k = 0; k < n; k++){
             printf(" *");
         }
     }
--- pattern/startri.c
+++ pattern/startri.c
@@ -2,7 +2,7 @@
 void main(){
     int n = 4;
     for(int i = 0; i < n; i){
-        for(int j = 0; j < n; j++){
+        for(int j = 0; j < i; j++){
             printf(" *");
         }
     }
